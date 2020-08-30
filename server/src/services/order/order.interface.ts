416bc74d0478beb703a450ba86e0494e5d566189
healthhazard <healthhazard@freenet.de>
2020-08-30 14:14:07 +0200

    server: add all possible ingredients
    While at it add an order_timestamp and comment in orderdb.

--- server/src/services/order/order.interface.ts
+++ server/src/services/order/order.interface.ts
@@ -1,8 +1,20 @@
 export interface IOrder {
   time: string;
+  order_timestamp: string;
   available: boolean;
   customer: string;
   phone: string;
   salami: boolean;
+  schinken: boolean;
+  thunfisch: boolean;
   mozzarella: boolean;
+  champignons: boolean;
+  paprika: boolean;
+  zwiebel: boolean;
+  mais: boolean;
+  oliven: boolean;
+  ananas: boolean;
+  peperoni_mild: boolean;
+  peperoni_scharf: boolean;
+  comment: string;
 }
